ef0c76f65cb82903337b1cbdb7e01ad49e3c5afb
Ian Robertson <Ianmr08@gmail.com>
2018-02-09 11:00:16 -0600

    added comments

--- parkStop.schema.js
+++ parkStop.schema.js
@@ -4,18 +4,26 @@ const Schema = mongoose.Schema;
 
 // Mongoose Schema
 const ImageSchema = new Schema ({
+    // every time a user submits an image it will POST the current park 
+    // ID along with the imgFile and the description if they provided one.
     parkId: {type: String, required: true},
     imgFile: {type: String, required: true},
     description: {type: String}
 });
 
 const ReviewSchema = new Schema ({
-    parkId: {type: String, required: true},
+    // every time a user submits a review it will POST the current park 
+    // ID along with the comment they made and the ratings
+    parkId: {type: String, required: true}, 
     comment: {type: String, required: true},
     Ratings: {type: Number, required: true}
 });
 
 const FavoriteSchema = new Schema ({
+    // this will take the parkID from the current park the user
+    // is on and POST the ID to the database. I will then have to GET the corresponding parks
+    // information to display it on the favorites Page. 
+    // (I don't know if there is an easier way. I feel like there is, I just can't think of it.) 
     parkId: {type: String, required: true} 
 });
 
